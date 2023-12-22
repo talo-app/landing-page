3521298af422b5fc526a1de88da464f8741310bc
Davidjustice28 <david.m.justice1@gmail.com>
2023-12-22 01:36:00 -0700

    create initial site

---
+++ index.js
@@ -0,0 +1,65 @@
+function initiateSite() {
+  let screenWidth = window.innerWidth;
+  let menuVisible = false;
+  
+  const burger = document.querySelector('.burger');
+  const menuItems = document.querySelector('.nav-links');
+  const aboutMenuLink = document.getElementById("about_link")
+  const featuresMenuLink = document.getElementById("features_link")
+  const pollMenuLink = document.getElementById("poll_link")
+  const waitlistForm = document.getElementById("home_content_cta_form")
+  const emailInput = document.getElementById("home_content_cta_input")
+
+  
+  displayBurgerOrMenuItems(screenWidth);
+  window.addEventListener('resize', () => {
+    screenWidth = window.innerWidth;
+    displayBurgerOrMenuItems(screenWidth);
+  })
+
+  aboutMenuLink.addEventListener('click', toggleMenu)
+  featuresMenuLink.addEventListener('click', toggleMenu)
+  pollMenuLink.addEventListener('click', toggleMenu)
+  waitlistForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = emailInput.value;
+    const emailRegex = /\S+@\S+\.\S+/;
+    if(!emailRegex.test(email)) {
+      alert("Please enter a valid email address.")
+      return;
+    }
+    emailInput.value = ""
+    // TODO: Send email to backend
+    alert("Thank you for submitting your email! If you are selected to participate in the beta, you will receive an email from us.")
+  
+  })
+
+
+  function toggleMenu() {
+    if(screenWidth < 600) {
+      if(!menuVisible) {
+        menuItems.style.display = 'flex'
+        menuVisible = true;
+      } else {
+        menuItems.style.display = 'none'
+        menuVisible = false;
+      }
+    }
+  }
+
+  function displayBurgerOrMenuItems(screenWidth) {
+    if(screenWidth > 600) {
+      burger.style.display = 'none'
+      menuItems.style.display = 'flex'
+  
+    } else {
+      menuItems.style.display = 'none'
+      burger.style.display = 'flex'
+    }
+
+    burger.addEventListener('click', toggleMenu);
+  }
+}
+
+
+initiateSite();
